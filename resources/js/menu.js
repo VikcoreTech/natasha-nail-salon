b6bc3060f11b11e03378cc10b56e72f15edfd9c1
VikcoreTech <alferov9500@gmail.com>
2025-05-12 11:22:56 -0500

    updated v2 hamburger

--- resources/js/menu.js
+++ resources/js/menu.js
@@ -1,6 +1,4 @@
-window.toggleMenu = function () {
-  const nav = document.getElementById('navMenu');
-  if (nav) {
-    nav.classList.toggle('active');
-  }
-};
+function toggleMenu() {
+  const navMenu = document.getElementById('navMenu');
+  navMenu.classList.toggle('show');
+}
